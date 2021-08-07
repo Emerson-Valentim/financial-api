8bfa25e5cf664ddd07d5de7687618e8515b8a58b
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-07 14:30:59 -0300

    fix: add autocall on throw function

--- app/Middleware/Authorizer.ts
+++ app/Middleware/Authorizer.ts
@@ -4,15 +4,15 @@ import HttpException from 'App/Exceptions/HttpException'
 
 export default class Authorizer {
   public async handle ({ request }: HttpContextContract, next: () => Promise<void>) {
-    await this.isAuthenticated(request) ?
+    this.isAuthenticated(request) ?
       await next() :
-      () => {
+      (() => {
         throw new HttpException('You cant access this route, verify your request headers', 401)
-      }
+      })()
   }
 
   private isAuthenticated (request) {
     const { 'x-api-key': xApiKey} = request.headers()
-    return xApiKey === Env.get('HEADER_API_KEY')|| request.url() === '/healthCheck'
+    return xApiKey === Env.get('HEADER_API_KEY') || request.url() === '/healthCheck'
   }
 }
