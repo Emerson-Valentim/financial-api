16ca4617e16f9b8d490211bed62fbd134797ab1e
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-07 13:41:53 -0300

    feat: add basic Authorizer tests

--- test/Middleware/Authorizer.spec.ts
+++ test/Middleware/Authorizer.spec.ts
@@ -1,7 +1,16 @@
 import test from 'japa'
+import supertest from 'supertest'
 
-test.group('Authorizer Middleware', (group) => {
-  test('Should call healthCheck and get 200', () => {
-    console.log(process.env.BASE_URL)
+test.group('Authorizer Middleware', () => {
+  test('Should call healthCheck and get 200', async () => {
+    await supertest(process.env.BASE_URL)
+      .get('/healthCheck')
+      .expect(200)
+  })
+
+  test('Should call authenticated route and get 401', async () => {
+    await supertest(process.env.BASE_URL)
+      .post('/category/load')
+      .expect(404)
   })
 })
