827cc5d4f3950b018dcb26731c817a7213043f0a
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-07 14:27:29 -0300

    fix: change implementation order and remove abstract class

--- start/kernel.ts
+++ start/kernel.ts
@@ -22,8 +22,8 @@ import Server from '@ioc:Adonis/Core/Server'
 */
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
-  () => import('App/Middleware/Authorizer'),
   () => import('App/Middleware/HttpLogger'),
+  () => import('App/Middleware/Authorizer'),
 ])
 
 /*
