91c74f626259af8c312fb171ca7c0f5139b755e3
Emerson Valentim <69153761+Emerson-Valentim@users.noreply.github.com>
2021-08-07 13:16:15 -0300

    Features/api logger (#6)
    * build: config new libs and aliases
    
    * chore: add new env.example var
    
    * chore: update git ignore
    
    * feat: add abstract controller class
    
    * feat: add database config
    
    * feat: add category model
    
    * feat: add category controller and validator
    
    * feat: add category migration
    
    * ci: fix task-definition name on last step
    
    * ci: fix cluster name
    
    * feat: add api Authorizer middleware
    
    * feat: add register to middleware
    
    * feat: add HttpLogger middleware
    
    * feat: add HttpException class
    
    * feat: implement generic ExceptionHandle
    
    * refactor: throw HttpError on unauthorized case
    
    * chore: add new env variables

---
+++ app/Middleware/HttpLogger.ts
@@ -0,0 +1,28 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Logger from '@ioc:Adonis/Core/Logger'
+
+export default abstract class HttpLogger {
+  public async handle (ctx: HttpContextContract, next: () => Promise<void>) {
+    await next()
+    HttpLogger.generateReponseLog(ctx)
+  }
+
+  public static generateReponseLog (ctx: HttpContextContract) {
+    const httpInfo = {
+      request: {
+        id: ctx.request.id(),
+        path: ctx.request.url(),
+        headers: ctx.request.headers(),
+        method: ctx.request.method(),
+        body: ctx.request.all(),
+        params: ctx.request.params(),
+      },
+      response: {
+        code: ctx.response.response.statusCode,
+        body: ctx.response.lazyBody[0],
+      },
+    }
+    Logger.info(httpInfo, 'Completed request')
+  }
+}
