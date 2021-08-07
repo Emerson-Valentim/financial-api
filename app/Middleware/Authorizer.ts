2dc93b526a01e2bb837ebf5784ac70502c1a1c16
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-07 13:12:24 -0300

    refactor: throw HttpError on unauthorized case

--- app/Middleware/Authorizer.ts
+++ app/Middleware/Authorizer.ts
@@ -1,12 +1,12 @@
 import Env from '@ioc:Adonis/Core/Env'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import HttpException from 'App/Exceptions/HttpException'
 
 export default class Authorizer {
-  public async handle ({request, response}: HttpContextContract, next: () => Promise<void>) {
+  public async handle ({ request }: HttpContextContract, next: () => Promise<void>) {
     const xApiKey = request.header('x-api-key')
     if(xApiKey !== Env.get('HEADER_API_KEY')) {
-      return response
-        .unauthorized({message: 'You cant access this route, verify your request headers'})
+      throw new HttpException('You cant access this route, verify your request headers', 401)
     }
     await next()
   }
