9152ec567a3b987fbe843f8488984f9011700d4f
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-07 13:12:07 -0300

    feat: implement generic ExceptionHandle

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -13,11 +13,17 @@
 |
 */
 
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor () {
     super(Logger)
+  }
+
+  public async handle (error: any, ctx: HttpContextContract) {
+    return super.handle(error, ctx)
   }
 }
