d89897f6b5a8c974411ccd649360449678333752
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 16:52:25 -0300

    fix: add initial and final date as required

--- app/Validators/BalanceValidator.ts
+++ app/Validators/BalanceValidator.ts
@@ -3,8 +3,8 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 export default class BalanceValidator {
   public countTotal () {
     let createdSchema = schema.create({
-      initial_date:  schema.date.optional({format: 'dd/LL/yyyy'}, [rules.requiredIfExists('final_date')]),
-      final_date:  schema.date.optional({format: 'dd/LL/yyyy'}, [rules.requiredIfExists('initial_date')]),
+      initial_date:  schema.date({format: 'dd/LL/yyyy'}),
+      final_date:  schema.date({format: 'dd/LL/yyyy'}),
       category_id: schema.number.optional([rules.exists({table: 'categories', column: 'id'})]),
     })
     return createdSchema
