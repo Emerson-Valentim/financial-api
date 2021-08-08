9196282e9a0d60d8ec026818c268fa01c510b902
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 16:31:34 -0300

    feat: change counttotal implementation to work without category

--- app/Services/Balance/CountTotal.ts
+++ app/Services/Balance/CountTotal.ts
@@ -2,19 +2,15 @@ import Category from 'App/Models/Category'
 import FinancialRelease from 'App/Models/FinancialRelease'
 
 interface CountInterface {
-  categoria: {
-    id_categoria: number
-    nome: string
-  },
   receita: string
   despesa: number
   saldo: number
 }
 
 type InputData = {
-  category_id: number
-  initial_date
-  final_date
+  category_id?: number
+  initial_date?
+  final_date?
 }
 
 export abstract class CountTotal {
@@ -22,27 +18,40 @@ export abstract class CountTotal {
     category_id: categoryId,
     initial_date: initialDate,
     final_date: finalDate}: InputData) {
-    const category = await Category.findOrFail(categoryId)
-
-    const query = this.queryFinancialRelease(categoryId)
+    let categoryModel
+    const query = this.queryFinancialRelease()
 
     if(initialDate && finalDate) {
       query.whereBetween('release_date', [initialDate, finalDate])
     }
 
-    const mappedResult = await this.toObject(category, query)
+    if(categoryId) {
+      query.where('sub_categories.category_id', categoryId)
+      categoryModel = await Category.findOrFail(categoryId)
+    }
+
+    const mappedResult = await this.toObject(query)
+
+    if(categoryModel) {
+      return {
+        categoria: {
+          id_categoria: categoryModel.id,
+          nome: categoryModel.name,
+        },
+        ...mappedResult,
+      }
+    }
 
     return mappedResult
   }
 
-  private static queryFinancialRelease (categoryId) {
+  private static queryFinancialRelease () {
     return FinancialRelease.query()
       .innerJoin('sub_categories', 'financial_releases.sub_category_id', 'sub_categories.id')
-      .where('sub_categories.category_id', categoryId)
       .select('financial_releases.value')
   }
 
-  private static async toObject (category, query): Promise<CountInterface> {
+  private static async toObject (query): Promise<CountInterface> {
     return (await query).reduce((mappedReturn: CountInterface, {$original: { value }}) => {
       value > 0 ?
         mappedReturn.receita += value:
@@ -50,10 +59,6 @@ export abstract class CountTotal {
       mappedReturn.saldo += value
       return mappedReturn
     }, {
-      categoria: {
-        id_categoria: category.id,
-        nome: category.name,
-      },
       receita:0,
       despesa:0,
       saldo: 0,
