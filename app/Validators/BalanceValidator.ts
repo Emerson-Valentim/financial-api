014d288f6f67cd6e13cec3f6fd0aab27cb516991
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 16:31:07 -0300

    feat: add category_id as optional on balance

--- app/Validators/BalanceValidator.ts
+++ app/Validators/BalanceValidator.ts
@@ -5,7 +5,7 @@ export default class BalanceValidator {
     let createdSchema = schema.create({
       initial_date:  schema.date.optional({format: 'dd/LL/yyyy'}, [rules.requiredIfExists('final_date')]),
       final_date:  schema.date.optional({format: 'dd/LL/yyyy'}, [rules.requiredIfExists('initial_date')]),
-      category_id: schema.number([rules.exists({table: 'categories', column: 'id'})]),
+      category_id: schema.number.optional([rules.exists({table: 'categories', column: 'id'})]),
     })
     return createdSchema
   }
