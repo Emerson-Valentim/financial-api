0c9c3d6e1315b2ae3c2973b194eba7e63bc81e7f
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-08 13:52:48 -0300

    feat: add load and loadbyid tests

--- test/Controller/Category.spec.ts
+++ test/Controller/Category.spec.ts
@@ -2,7 +2,15 @@ import test from 'japa'
 import supertest from 'supertest'
 
 test.group('Category controller', () => {
-  test('Should call create and recieve 201', async () => {
+  const createCategory = ({ name }) => {
+    return supertest(process.env.BASE_URL)
+      .post('/category/create')
+      .send({name})
+      .set('x-api-key', process.env.HEADER_API_KEY)
+      .expect(201)
+  }
+
+  test('Should call create and receive 201', async () => {
     const validCategory = {
       name: 'Category 1',
     }
@@ -14,7 +22,7 @@ test.group('Category controller', () => {
       .expect(201)
   })
 
-  test('Should call create and recieve 400', async () => {
+  test('Should call create and receive 400', async () => {
     const validCategory = {
       invalidField: 'Category 1',
     }
@@ -24,5 +32,22 @@ test.group('Category controller', () => {
       .send(validCategory)
       .set('x-api-key', process.env.HEADER_API_KEY)
       .expect(400)
+  })
+
+  test('Should call load and receive 200', async (assert) => {
+    const { body: model } = await createCategory({name: 'LoadTest'})
+
+    const { body: loadAll } = await supertest(process.env.BASE_URL)
+      .get('/category/load')
+      .set('x-api-key', process.env.HEADER_API_KEY)
+      .expect(200)
+
+    const { body: loadById } = await supertest(process.env.BASE_URL)
+      .get(`/category/load/${model.id}`)
+      .set('x-api-key', process.env.HEADER_API_KEY)
+      .expect(200)
+
+    assert.isAtLeast(loadAll.length, 1)
+    assert.equal(loadById.id, model.id)
   })
 })
