be3b28d9a96806065908e92b76c45f86627d5981
Emerson Valentim <69153761+Emerson-Valentim@users.noreply.github.com>
2021-08-08 15:24:45 -0300

    fix: change interface and toObject function (#13)

--- app/Services/Balance/CountTotal.ts
+++ app/Services/Balance/CountTotal.ts
@@ -5,7 +5,10 @@ interface CountInterface {
   categoria: {
     id_categoria: number
     nome: string
-  }
+  },
+  receita: string
+  despesa: number
+  saldo: number
 }
 
 type InputData = {
@@ -36,21 +39,24 @@ export abstract class CountTotal {
     return FinancialRelease.query()
       .innerJoin('sub_categories', 'financial_releases.sub_category_id', 'sub_categories.id')
       .where('sub_categories.category_id', categoryId)
-      .groupBy('sub_categories.id')
-      .select([
-        'sub_categories.id as sub_category_id',
-        'sub_categories.name as sub_category_name',
-      ])
-      .count('*')
+      .select('financial_releases.value')
   }
 
   private static async toObject (category, query): Promise<CountInterface> {
-    return (await query).reduce((mappedReturn: CountInterface, {$extras}) => {
-      if(!mappedReturn[$extras.sub_category_name]) {
-        mappedReturn[$extras.sub_category_name] = 0
-      }
-      mappedReturn[$extras.sub_category_name] += +$extras.count
+    return (await query).reduce((mappedReturn: CountInterface, {$original: { value }}) => {
+      value > 0 ?
+        mappedReturn.receita += value:
+        mappedReturn.despesa += value * -1
+      mappedReturn.saldo += value
       return mappedReturn
-    }, {categoria: {id_categoria: category.id,nome: category.name}})
+    }, {
+      categoria: {
+        id_categoria: category.id,
+        nome: category.name,
+      },
+      receita:0,
+      despesa:0,
+      saldo: 0,
+    })
   }
 }
